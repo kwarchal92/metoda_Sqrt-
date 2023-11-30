ac5c60a9211eda977efc09390b8defa7cc95c46b
kwarchal <kamilwarchal295@gmail.com>
2023-11-30 19:37:35 +0100

    metoda Sqrt()

---
+++ src/metoda_sqrt/MetodaSqrt.java
@@ -0,0 +1,12 @@
+package metoda_sqrt;
+
+public class MetodaSqrt {
+
+    public static void main(String[] args){
+        System.out.println("Program wyswietla pierwiastek kwadratowy \nz liczby PI z dokladnoscia do dwoch miejsc po przecinku");
+        //System.out.println("Z liczby PI z dokladnoscia do dwoch miejsc po przecinku");
+
+        System.out.printf("Sqrt(PI) = "+" %2.2f.\n", Math.sqrt(Math.PI));
+    }
+
+}
